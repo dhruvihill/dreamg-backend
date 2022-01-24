5bf1d2193e9182ded230f2ae82e3bba2e87fcb54
dhruv <dhruvihill@gmail.com>
2022-01-24 17:19:59 +0530

    testing purpose host setup

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -28,7 +28,7 @@ router.get("/", verifyUser, async (req, res) => {
     const isNotification = await fetchData(isNotificationQuery, [userId]);
     if (upcomingMatches.length > 0) {
       const { data } = await axios({
-        url: "localhost:3000/api/v1/prediction/getTrendingPredictors",
+        url: "https://dreamg-backend.herokuapp.com/api/v1/prediction/getTrendingPredictors",
         headers: { authtoken },
       });
       // const start = Date.now();
