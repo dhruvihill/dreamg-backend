eceb9afdfc2b2ab0314adcc082c34536dda0f45c
dhruv <dhruvihill@gmail.com>
2022-01-25 08:58:20 +0530

    testing purpose host setup

--- database/db_connection.js
+++ database/db_connection.js
@@ -9,5 +9,10 @@ const connection = mysql.createConnection({
   password: process.env.PASSWORD || "TaB58BF8up543Ck7rYCr",
   database: process.env.DATABASE || "brbogw76w7fnqwls61xz",
 });
-
+console.log(
+  process.env.HOST || "brbogw76w7fnqwls61xz-mysql.services.clever-cloud.com",
+  process.env.USER || "utrcdzbg6f5vhtg0",
+  process.env.PASSWORD || "TaB58BF8up543Ck7rYCr",
+  process.env.DATABASE || "brbogw76w7fnqwls61xz"
+);
 module.exports = connection;
