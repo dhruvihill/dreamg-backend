75c49891fe00a8faf4d12521c213c94f50f1155a
dhruv <dhruvihill@gmail.com>
2022-01-25 09:37:56 +0530

    testing purpose host setup

--- database/db_connection.js
+++ database/db_connection.js
@@ -2,16 +2,18 @@ const mysql = require("mysql");
 require("dotenv/config");
 
 // Creating Connection
+
+//
 const connection = mysql.createConnection({
-  host: process.env.DATABASE_HOST || process.env.CLEVER_CLOUD_HOST,
-  user: process.env.DATABASE_USER || process.env.CLEVER_CLOUD_USER,
-  password: process.env.DATABASE_PASSWORD || process.env.CLEVER_CLOUD_PASSWORD,
-  database: process.env.DATABASE_NAME || CLEVER_CLOUD_DATABASE_NAME,
+  host: process.env.CLEVER_CLOUD_HOST,
+  user: process.env.CLEVER_CLOUD_USER,
+  password: process.env.CLEVER_CLOUD_PASSWORD,
+  database: process.env.CLEVER_CLOUD_DATABASE_NAME,
 });
 console.log(
-  process.env.DATABASE_HOST || process.env.CLEVER_CLOUD_HOST,
-  process.env.DATABASE_USER || process.env.CLEVER_CLOUD_USER,
-  process.env.DATABASE_PASSWORD || process.env.CLEVER_CLOUD_PASSWORD,
-  process.env.DATABASE_NAME || CLEVER_CLOUD_DATABASE_NAME
+  process.env.CLEVER_CLOUD_HOST,
+  process.env.CLEVER_CLOUD_USER,
+  process.env.CLEVER_CLOUD_PASSWORD,
+  process.env.CLEVER_CLOUD_DATABASE_NAME
 );
 module.exports = connection;
