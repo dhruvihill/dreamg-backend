3c8adbec8c21b9c9eb14b85fda65fe1d5e3dd8d8
dhruv <dhruvihill@gmail.com>
2022-01-25 11:34:08 +0530

    exception handle on database server disconnect

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get("*", (req, res) => {
 
 // Listening App
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${process.env.PORT || 3000}`);
+  console.log(`Listening on ports ${process.env.PORT || 3000}`);
 });
 
 module.exports = app;
