ae76cd80c63f0d0121531ba81625677e6b4dc7d1
dhruvihill <dhruvihill@gmail.com>
2022-02-23 16:25:31 +0530

    isMegaContestCreated and isHeadToHeadCreated flags added in dashboard data

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -16,14 +16,14 @@ router.get("/", verifyUser, async (req, res) => {
   //   "SELECT COUNT(*) > 0 AS isNotification FROM notifications JOIN notification_history ON notification_history.userId = notifications.userId WHERE notifications.userId = ? AND notifications.creationTime > notification_history.lastTimeCalled;";
 
   const isNotificationQuery =
-    "SELECT EXISTS(SELECT notificationId FROM `fullnotification` WHERE fullnotification.userId = 9 AND lastTimeCalled < creationTime) AS isNotification;";
+    "SELECT EXISTS(SELECT notificationId FROM `fullnotification` WHERE fullnotification.userId = ? AND lastTimeCalled < creationTime) AS isNotification;";
   const upcomingMatchesQuery =
-    "SELECT seriesName, seriesDname, matchId, matchTypeId, matchTyprString, matchStartTimeMilliSeconds, matchStartDateTime, matchStatus, venue, displayName, team1Id, team1Name, team1DisplayName, team1FlagURL, team2Id, team2Name, team2DisplayName, team2FlagURL FROM fullmatchdetails WHERE matchStatus = 1 ORDER BY matchStartTimeMilliSeconds DESC LIMIT 5;";
+    "SELECT seriesName, seriesDname, matchId, matchTypeId, matchTyprString, matchStartTimeMilliSeconds, matchStartDateTime, matchStatus, venue, displayName, team1Id, team1Name, team1DisplayName, team1FlagURL, team2Id, team2Name, team2DisplayName, team2FlagURL, EXISTS(SELECT fullteamdetails.userTeamId FROM fullteamdetails WHERE fullteamdetails.userId = ? AND fullteamdetails.matchId = fullmatchdetails.matchId AND fullteamdetails.teamTypeString = 'HEAD_TO_HEAD') AS isHeadToHeadCreated, EXISTS(SELECT fullteamdetails.userTeamId FROM fullteamdetails WHERE fullteamdetails.userId = ? AND fullteamdetails.matchId = fullmatchdetails.matchId AND fullteamdetails.teamTypeString = 'MEGA_CONTEST') AS isMegaContestCreated FROM fullmatchdetails WHERE matchStatus = 1 ORDER BY matchStartTimeMilliSeconds DESC LIMIT 5;";
 
   try {
     const [upcomingMatches, isNotification] = await fetchData(
       `${upcomingMatchesQuery}${isNotificationQuery}`,
-      [userId]
+      [userId, userId, userId]
     );
     const { data } = await axios({
       url: `${req.protocol}://${req.headers.host}/api/v1/prediction/getTrendingPredictors`,
