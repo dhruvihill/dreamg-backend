c49de9111bdb3bd8dcc603dea3e61fe64b2e74a6
Dhruv <dhruvihill@gmail.com>
2022-04-11 21:15:11 +0530

    created date formater according to UTC +05:30

--- middleware/convertTimeZone.js
+++ middleware/convertTimeZone.js
@@ -9,11 +9,12 @@ const convertTimeZone = (time, timeZone) => {
 
     if (timeZone.includes("00:00") || timeZone === "") {
       const newTime = moment(time).utc().format();
-      const newTime2 = newTime.replace("Z", "+00:00");
+      const newTime2 = newTime.replace("Z", "");
       return [newTime2, new Date(newTime).getTime().toString()];
     }
     const newTime = moment(time).utcOffset(timeZone).format();
-    return [newTime, new Date(newTime).getTime().toString()];
+    const newTime2 = newTime.replace(timeZone, "");
+    return [newTime2, new Date(newTime).getTime().toString()];
   } else {
     return [null, null];
   }
